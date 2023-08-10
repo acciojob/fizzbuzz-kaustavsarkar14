94f6485cdcecd65ae52b6f7d6d5869b8765d723b
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-08-10 11:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let res = ""
+for(let i=1; i<=100; i++){
+	if( i%3===0 && i%5===0)
+		res = res + "FizzBuzz" + "\n" ;
+	else if(i%5===0)
+		res = res + "Buzz" + "\n" ;
+	else if (i%3===0)
+		res = res + "Fizz" + "\n" ;
+	else
+	res = res + i + "\n" ;
+}
+
+alert(res)
